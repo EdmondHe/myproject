dfc65b616b48f871055d2897ad78e7efd4def85d
helei <767306399@qq.com>
2018-11-25 10:54:45 +0800

    add RocketMQHealthIndicator class

--- src/main/java/org/acme/myproject/controller/HelloController.java
+++ src/main/java/org/acme/myproject/controller/HelloController.java
@@ -1,10 +1,16 @@
 package org.acme.myproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.actuate.metrics.*;
+import org.springframework.boot.actuate.health.*;
+
+import javax.management.monitor.CounterMonitor;
 
 @RestController
 public class HelloController {
+
 
     @RequestMapping("/helei")
     public String index(){
